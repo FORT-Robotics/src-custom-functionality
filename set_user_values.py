bacb76efe1128598ddab49e90a368190c29e4153
Quinn McHugh <quinn.mchugh@fortrobotics.com>
2022-05-09 14:47:24 +0000

    Added docstrings to functions and refactored function names.

--- set_user_values.py
+++ set_user_values.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
+"""Python 2 module for setting customer values on an FORT Robotics Safety Remote Contorller (SRC)."""
 ​
-def requestSetUserValue(key, value):
+def set_user_value(key, value):
+    """Set a specified key to a specified integer value.
+    
+    Args:
+        key (int): The key to set.
+        value (int): The new value of the key.
+    """
     if key <= 0xff:
         maskedValue = int(value) & 0xffffffff
         valueStr = "{:02x}{:02x}{:02x}{:02x}".format(
@@ -12,7 +19,13 @@ def requestSetUserValue(key, value):
         messageString = "{:02x}".format(key) + valueStr + "FFFFFF\r"
         print "Set key {} to {}".format(key, value) + " :: " + messageString
 ​
-def requestSetUserStringValue(key, string):
+def set_user_str_value(key, string):
+    """Set a specified key to a specified string value.
+    
+    Args:
+        key (int): The key to set.
+        value (int): The new value of the key.
+    """
     if key <= 0xff:
         string = string[:18] # truncate to 18 characters max
         
@@ -25,8 +38,8 @@ def requestSetUserStringValue(key, string):
             print "Set key {} to '{}', segment {} ('{}') :: {}".format(key, string, segment, chunk, messageString)
             segment += 1
 ​
-requestSetUserValue(99, 1)
-requestSetUserStringValue(90, "This is line 1")
-requestSetUserStringValue(91, "Second line")
-requestSetUserStringValue(92, "Line number 3")
-requestSetUserStringValue(93, "#4:Fourth line 4")
+set_user_value(99, 1)
+set_user_str_value(90, "This is line 1")
+set_user_str_value(91, "Second line")
+set_user_str_value(92, "Line number 3")
+set_user_str_value(93, "#4:Fourth line 4")
